12f4bca4634d23033269d04cf3429e1345e34617
anhthuus1982 <info.anhthu@gmail.com>
2025-03-11 15:50:44 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
 import sqlite3
 import pandas as pd
+import os
+import time
 
 class Database:
     def __init__(self, db_name='trading_data.db'):
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
         self.create_tables()
+        self.backup_database()
 
     def create_tables(self):
         self.cursor.execute('''
@@ -20,6 +23,13 @@ class Database:
         ''')
         self.cursor.execute('CREATE INDEX IF NOT EXISTS idx_timestamp ON trades (timestamp)')
         self.conn.commit()
+
+    def backup_database(self):
+        backup_name = f"backup_{time.strftime('%Y%m%d')}.db"
+        if not os.path.exists(backup_name):
+            with open(backup_name, 'w') as f:
+                for line in self.conn.iterdump():
+                    f.write('%s\n' % line)
 
     def save_trade(self, timestamp, symbol, side, price, amount):
         try:
@@ -43,6 +53,11 @@ class Database:
 
     def get_trades(self):
         return pd.read_sql_query("SELECT * FROM trades", self.conn)
+
+    def delete_old_trades(self, days=30):
+        cutoff = time.strftime('%Y-%m-%d', time.gmtime(time.time() - days * 86400))
+        self.cursor.execute("DELETE FROM trades WHERE timestamp < ?", (cutoff,))
+        self.conn.commit()
 
     def close(self):
         self.conn.close()
